acc3b720cb2ac40ca4829cf387651664d878c98a
zhernandez11 <144408706+zhernandez11@users.noreply.github.com>
2023-09-27 03:39:26 +0000

    file

---
+++ class work/class6.cpp
@@ -0,0 +1,21 @@
+//Write a C++ program that uses a `while` loop to print all the multiples of 7 between 50 and 200.
+#include<iostream>
+using namespace std;
+
+/*int main(){
+    int i = 50;
+    while (i <= 200){
+        if (i%7==0){
+
+            cout << i << "\n";
+        }
+        i++;
+    }
+}*/
+int main(){
+    int i = 56;
+    while (i <= 200){
+        cout << i << "\n";
+        i +=7;
+    }
+}
