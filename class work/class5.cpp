a262ac6632e446cc083cd9117215b7a998a111fb
zhernandez11 <144408706+zhernandez11@users.noreply.github.com>
2023-09-27 03:10:34 +0000

    file

--- class work/class5.cpp
+++ class work/class5.cpp
@@ -1,12 +1,16 @@
-//Create a program that asks the user for a number and prints whether it's a prime number or not. Use a loop to check divisibility.
-
+/*
+Develop a C++ program that uses a `while` loop to calculate and print the sum of the squares of the first 15 positive integers (1^2 + 2^2 + 3^2 + ... + 15^2).
+*/
 #include<iostream>
 using namespace std;
 
 int main(){
-    cout << "Enter any number:";
-    int num;
-    cin >> num;
-    
-    
+    int i = 1;
+    int sum = 0;
+    while (i <= 15){
+        sum +=(i * i);
+        //sum = sum + (i * i);
+        i++;
+    }
+    cout << sum << "\n";
 }
